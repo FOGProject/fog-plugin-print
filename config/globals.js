b37deabfa7ca389741a9181117a928123fff6c65
Tom Elliott <tommygunsster@gmail.com>
2020-05-12 10:34:35 -0500

    Similar to snapin, inject printer menu where we want

--- config/globals.js
+++ config/globals.js
@@ -1,24 +1,32 @@
-module.exports.globals = {
-  menuItems: [
+const array_insert_before = function(text, item, insert) {
+  let index = undefined;
+  for (var _i = 0, _len = item.length;_i < _len;_i++) {
+    if (item[_i].text === text) {
+      index = _i;
+      break;
+    }
+  }
+  item.splice(index, 0, insert);
+  return item;
+};
+let menuItems = {
+  text: 'Printer',
+  plural: 'Printers',
+  link: '/printers',
+  icon: 'fa-print',
+  subLinks: [
     {
-      text: 'Printer',
-      plural: 'Printers',
-      link: '/printers',
-      icon: 'fa-print',
-      subLinks: [
-        {
-          link: '/printers/create',
-          text: 'Create New Printer'
-        },
-        {
-          link: '/printers/export',
-          text: 'Export Printers'
-        },
-        {
-          link: '/printers/import',
-          text: 'Import Printers',
-        }
-      ]
+      link: '/printers/create',
+      text: 'Create New Printer'
+    },
+    {
+      link: '/printers/export',
+      text: 'Export Printers'
+    },
+    {
+      link: '/printers/import',
+      text: 'Import Printers',
     }
   ]
 };
+sails.config.globals.menuItems = array_insert_before('Task', sails.config.globals.menuItems, menuItems);
